0d795ffc67fe027ac17a48ce707f554c0ad3c866
Naveen-rvv123 <89374031+Naveen-rvv123@users.noreply.github.com>
2021-10-23 11:56:54 +0530

    Add files via upload

---
+++ LinqDemoApp/LinqDemoApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
